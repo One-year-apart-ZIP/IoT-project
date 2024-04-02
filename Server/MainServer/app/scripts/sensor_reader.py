c661d000520fbbb275eea78e12719bb4c00b6382
ayeong3000 <choiayeong1229@gmail.com>
2024-04-02 17:24:46 +0900

    add server

--- Server/MainServer/app/scripts/sensor_reader.py
+++ Server/MainServer/app/scripts/sensor_reader.py
@@ -2,11 +2,12 @@ import serial
 import requests
 import json
 
-dummy_data = '{"Temp": "18.3", "Hum": "55.5", "Gas": "05.3"}'
-print(dummy_data)
+ser = serial.Serial('/dev/ttyS0', 9600)
 
-# ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
-# data = ser.readline().decode('utf-8').rstrip()
-parsed_data = json.loads(dummy_data)
-response = requests.post('http://192.168.0.59:5000/api/receive-energy-data', json=parsed_data)    
-print(response.json())
+while 1:
+	data = ser.readline().decode('utf-8').rstrip()
+	if data is not None:
+		parsed_data = json.loads(data)
+		print(parsed_data)
+		response = requests.post('http://192.168.137.91:5000/api/receive-energy-data', json=parsed_data)
+		print(response.json())
--- Server/MainServer/app/views.py
+++ Server/MainServer/app/views.py
@@ -1,9 +1,12 @@
-from flask import render_template, request, redirect, url_for, flash, send_from_directory, jsonify, Flask
+from flask import session,render_template, request, redirect, url_for, flash, send_from_directory, jsonify, Flask
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from app import app,db
 from app.models import EnergyData, UploadedFile
 import os
+import serial
+
+ser = serial.Serial('/dev/ttyS0', 9600)
 
 UPLOAD_FOLDER = '/home/pi/IoT_Project/app/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'avi'}
@@ -60,13 +63,16 @@ def uploaded_file(filename):
 def receive_energy_data():
     data = request.get_json()
     if not data:
-        return jsonify({"message": "No data provided"}), 400
+       return jsonify({"message": "No data provided"}), 400
 
     try:
         new_energy_data = EnergyData(
             Temp=data['Temp'],
             Hum=data['Hum'],
-            Gas=data['Gas']
+            Gas=data['Gas'],
+            ac=data['ac'],
+            bo=data['bo'],
+            hu=data['hu']
         )
         db.session.add(new_energy_data)
         db.session.commit()
@@ -78,11 +84,26 @@ def receive_energy_data():
     except Exception as e:
         return jsonify({"message": str(e)}), 500
 
+"""@app.route('/api/latest-energy-data')
+def latest_energy_data():
+    latest_data = EnergyData.query.order_by(EnergyData.curDate.desc()).first()
+    if latest_data:
+        return jsonify({
+           'curDate' : latest_data.curDate.strftime("%Y-%m-%d %H:%M:%S"),
+           'Temp' : latest_data.Temp,
+           'Hum' : latest_data.Hum,
+	   'Gas' : latest_data.Gas,
+           'ac' : session.get('ac', 'N/A'),
+           'bo' : session.get('bo', 'N/A'),
+           'hu' : session.get('hu', 'N/A')
+        })
+    else:
+       return jsonify({'message' : 'No data available'}), 404
+"""
 # additional sentence
 @app.route('/api/latest-file', methods=['GET'])
 def get_latest_file():
     files = os.listdir(UPLOAD_FOLDER)
-
     if not files:
         return jsonify({'message' : 'No files found'}), 404
 
@@ -93,3 +114,74 @@ def get_latest_file():
     latest_file_timestamp = datetime.fromtimestamp(os.path.getmtime(latest_file_path)).strftime('%Y-%m-%d %H:%M:%S')
 
     return jsonify({'filename' : latest_file, 'timestamp' : latest_file_timestamp})
+
+DEVICE_SIGNALS = {
+    'aircon_on': '0',
+    'aircon_off': '1',
+    'boiler_on': '2',
+    'boiler_off': '3',
+    'humidifier_on': '4',
+    'humidifier_off': '5',
+}
+
+@app.route('/control-device', methods=['POST'])
+def control_device():
+    content = request.json
+    command = content.get('command')
+    signal = DEVICE_SIGNALS.get(command)
+
+    if signal:
+        ser.write(signal.encode())
+        return {'status': 'success', 'signal_snet': signal},200
+    else:
+        return {'status': 'error', 'message': 'Invalid command'}, 400
+
+@app.route('/delete-file/<filename>', methods=['POST'])
+def delete_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        flash(f'{filename} has been deleted from server.', 'success')
+    else:
+        flash(f'{filename} not found on server.', 'error')
+
+
+    file_record = UploadedFile.query.filter_by(filename=filename).first()
+    if file_record:
+        db.session.delete(file_record)
+        db.session.commit()
+        flash(f'{filename} record has been deleted from database.', 'success')
+
+    return redirect(url_for('door_security'))
+
+def delete_all_photos():
+    directory = os.path.join(app.config['UPLOAD_FOLDER'])
+    errors = []
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            errors.append(f"Error deleting file {filename}: {str(e)}")
+
+    if not errors:
+        try:
+            num_rows_deleted = db.session.query(UploadedFile).delete()
+            db.session.commit()
+            flash(f"All photos and database records deleted. {num_rows_deleted} records removed.", "success")
+        except Exception as e:
+            db.session.rollback()
+            flash(f"Error deleting records from database: {str(e)}", "error")
+    else:
+        for error in errors:
+            flash(error, "error")
+
+@app.route('/delete-all-photos', methods=['POST'])
+def delete_all_photos_route():
+    delete_all_photos()
+    return redirect(url_for('door_security'))
